884ab4e2d74d668ce22f90b93ff946e9757f3eec
ohood84 <obeidat.ohood@gmail.com>
2019-09-10 11:00:36 -0700

    add last chanfes in portfolio desighn and colors

---
+++ js/indexScripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  // $("#space").hide();
+  var x=1;
+
+  // $("#space").text(" "+"  "+" ");
+  // $("#5").text(" "+" F");
+  // // while(x=1){
+    for(var i=1;i<=10;i++){
+         $("#"+i+"").fadeToggle(i*1000);}
+    for(var i=1;i<10;i++){
+        $("#"+i+"").fadeToggle(i*1000);}
+
+  x=2;
+ // }
+
+
+
+
+})
---
+++ js/projectsScripts.js
@@ -0,0 +1,10 @@
+$(documment).ready(function(){
+   // $("#space").hide();
+   //  for(var i=1;i<10;i++){
+   //       $("#"+i+"").fadeToggle(i*1000);}
+   //   for(var i=1;i<10;i++){
+   //      $("#"+i+"").fadeToggle(i*1000);}
+
+
+
+})
